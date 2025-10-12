e1c94757ae42608f099cf2d5605e91015d49f478
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-10-12 22:13:30 +0700

    Lab 7 | Entity

---
+++ src/main/java/ndyudev/lab7/slider/entity/Departments.java
@@ -0,0 +1,16 @@
+package ndyudev.lab7.slider.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Departments {
+	private String id;
+	private String name;
+	private String description;
+}
+
+
